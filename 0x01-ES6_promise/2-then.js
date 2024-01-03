8fb0cb66eb76afb3455fc28a993e4f30f52ca5fc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-03 23:23:52 +0100

    using finaly to resolve

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  const successHandler = () => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  };
+  const successHandler = () => ({ status: 200, body: 'Success' });
 
   const errorHandler = () => new Error();
+  const finallyHandler = () => console.log('Got a response from the API');
 
   return promise
     .then(successHandler)
-    .catch(errorHandler);
+    .catch(errorHandler)
+    .finally(finallyHandler);
 }
