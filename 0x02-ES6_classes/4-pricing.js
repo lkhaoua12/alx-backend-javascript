4d410c18faf828543d2e04a28e5bd1ee3e14fff5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-05 00:53:53 +0100

    using static methods

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -27,7 +27,7 @@ export default class Pricing {
     return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
-  convertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
