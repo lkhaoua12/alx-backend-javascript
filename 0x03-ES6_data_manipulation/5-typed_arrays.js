766f6afea8247ae250dd4ae5848a260e68fd00fb
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-10 00:30:56 +0100

    usnig typed array to access mem

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position can\'t be greater than length');
+  }
+
   const arrayBuff = new ArrayBuffer(length);
   const arrayView = new Int8Array(arrayBuff);
   arrayView.fill(value, position, position + 1);
