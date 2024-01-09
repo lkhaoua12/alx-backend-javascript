a7904b60c5c074b3f33a69aa9d09cc610c8b3074
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-09 13:21:55 +0100

    using map combined with filter

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+export default function updateStudentGradeByCity(students = [], city = '', newGrades = []) {
+  const updatedStudents = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const updatedGrade = newGrades.find((std) => std.studentId === student.id);
+      if (updatedGrade) {
+        student.grade = updatedGrade.grade;
+      } else {
+        student.grade = 'N/a';
+      }
+
+      return student;
+    });
+
+  return updatedStudents;
+}
