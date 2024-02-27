a356a7473ce8b16b3d682b17b1be3494e74de215
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-02-28 00:27:39 +0100

    semistandar fixed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,13 +1,3 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
 const displayMessage = (message) => console.log(message);
 
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
 module.exports = displayMessage;
